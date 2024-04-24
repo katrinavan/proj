b865ef15f49e0b8ded33b2e98284f21af167e821
katrinavan <katrinavan@berkeley.edu>
2024-04-23 20:29:20 -0700

    lo

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,49 +1,21 @@
-import React from "react";
-import "./App.css";
-
-const Header = () => {
-    return (
-        <header>
-            <h1>Berkeley Blog</h1>
-            <nav>
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </nav>
-        </header>
-    );
-};
-
-const BlogPost = ({ title, content }) => {
-    return (
-        <article>
-            <h2>{title}</h2>
-            <p>{content}</p>
-        </article>
-    );
-};
-
-const Footer = () => {
-    return (
-        <footer>
-            <p>Copyright © 2023 Berkeley Blog. All rights reserved.</p>
-        </footer>
-    );
-};
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import './App.css';
 
 const App = () => {
-    return (
-        <div className="app">
-            <Header />
-            <main>
-                <BlogPost title="Welcome to Berkeley Blog" content="This is your first post on the Berkeley Blog." />
-                {/* Add more <BlogPost /> components as needed */}
-            </main>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="App">
+      <header>
+        {/* Your site's header here */}
+      </header>
+      <main>
+        <Outlet /> {/* This is where the routed pages will be displayed */}
+      </main>
+      <footer>
+        {/* Your site's footer here */}
+      </footer>
+    </div>
+  );
 };
 
 export default App;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Ensure this path matches the location of your App.jsx file
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Welcome from './pages/Welcome';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="welcome" element={<Welcome />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
